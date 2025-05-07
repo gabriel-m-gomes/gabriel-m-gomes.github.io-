34f31d9e20215c8905850228f4afcb1d0f810bee
Gabriel Macedo <gabriel007.macedo@gmail.com>
2025-05-07 00:02:33 -0300

    implement section navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import { useState } from "react";
-import Hero from "./components/Hero";
+import Hero from "./sections/Hero";
+import Navbar from "./sections/NavBar";
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
+      <Navbar />
       <Hero />
     </>
   );
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,17 +1,17 @@
-function Hero() {
+const Hero = () => {
   return (
-    <div className="bg-[#0f172a] text-[#c2e8f0] px-60 py-20">
+    <div id="hero-container" className=" text-[#c2e8f0] px-60 py-20">
       <div className="space-y-2">
         <p className="font-medium font-sans text-sm">Hello, my name is</p>
         <h1 className="text-5xl font-bold tracking-tight leading-tight font-sans">
           Gabriel Macedo.
         </h1>
         <h2 className="text-1xl font-medium font-sans">
-          I am, Back End Developer.
+          I am Back End Developer.
         </h2>
       </div>
     </div>
   );
-}
+};
 
 export default Hero;
