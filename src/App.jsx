fd17836acc076944daa6e9c36d0b4a07e84aca3f
Gabriel Macedo <gabriel007.macedo@gmail.com>
2025-05-08 14:31:16 -0300

    implemented section contact and add functions for icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Hero from "./sections/Hero";
 import Navbar from "./sections/NavBar";
+import Contact from "./sections/Contact";
+
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Contact />
     </>
   );
 }
