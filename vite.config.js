a69881771d1965b75346eee8af3ae1a3ffe350db
Gabriel Macedo <gabriel007.macedo@gmail.com>
2025-09-01 21:16:19 -0300

    Corrige base do vite.config para deploy no GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/gabriel-m-gomes.github.io/",
+  base: "/gabriel-m-gomes.github.io-/", // observe o hífen no final
   plugins: [tailwindcss(), react()],
 });
