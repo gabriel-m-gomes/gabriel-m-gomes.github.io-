563105e86872d91ca17a70b13349d59a96fd209f
Gabriel Macedo <gabriel007.macedo@gmail.com>
2025-09-01 21:26:25 -0300

    Deploy site

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/gabriel-m-gomes.github.io-/", // observe o hífen no final
+  base: "/",
   plugins: [tailwindcss(), react()],
 });
